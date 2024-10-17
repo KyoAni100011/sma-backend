b1f0c84a49bd91b17300d859e6ae1b62a979ab0e
kha <hoangkhatruong2003@gmail.com>
2024-10-17 22:57:32 +0700

    Setup cors

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import bodyParser from "body-parser";
 import userRouter from './routes/user.route'
+import cors from 'cors'
 
 const app = express();
+app.use(cors())
 
 
 app.use(bodyParser.json());
